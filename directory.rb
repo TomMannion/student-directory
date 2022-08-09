940e14edb642ba5c3074387d5fe7918fd05bf24f
postie89 <t.mannion89@gmail.com>
2022-08-09 15:12:52 +0100

    In Directory.rb Added Interactive_menu to add a user interface. Changed students array to be global to avoid passing between methods. Refactored code to reduce repetition in interactive_menu

--- directory.rb
+++ directory.rb
@@ -1,38 +1,70 @@
+@students = [] # an empty array accessable by all methods
+
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  # create empty array
-  students = []
   # get the first name
   name = gets.chomp
   # while the name is not empty, repeat this code
   while !name.empty? do
     # add the student has to the array
-    students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students"
+    @students << {name: name, cohort: :november}
+    puts "Now we have #{@students.count} students"
     # get another name from the user
     name = gets.chomp
   end
   # return array of students
-  students
+  @students
 end
 
 def print_header
-  puts "The students of Villains Academy"
-  puts "-------------"
+  puts "The students of Villains Academy".center(40)
+  puts "-------------".center(40)
 end
 
-def print(names)
-  names.each do |name|
-    puts "#{name[:name]} (#{name[:cohort]} cohort)"
+def print_student_list
+  @students.each_with_index do |name, index|
+    puts "#{index + 1}. #{name[:name]} (#{name[:cohort]} cohort)".center(40)
   end
 end
 
-def print_footer(names)
+def print_footer
   # finally, we puts the total number of students
-  puts "Overall, we have #{names.count} great students"
+  puts "Overall, we have #{@students.count} great students"
 end
-students = input_students
-print_header
-print(students)
-print_footer(students)
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+end
+
+def show_students
+  print_header
+  print_student_list
+  print_footer
+end
+
+def process(selection)
+  case selection
+  when "1"
+    students = input_students
+  when "2"
+    show_students
+  when "9"
+    exit
+  else
+    puts "I don't know what you meant, try again"
+  end
+end
+
+def interactive_menu
+  loop do
+    # printing menu and asking user for input
+    print_menu
+    process(gets.chomp)
+    # do what user has selected
+  end
+end
+
+interactive_menu
